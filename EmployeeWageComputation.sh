d9b5b27bb4be349ed2f040669b057c5d6bfd2338
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-26 01:59:01 +0530

    Calculating Wage for month

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -6,8 +6,8 @@ echo "Welcome To EmployeeWageComputaion program"
 IS_EMPLOYEE_PRESENT=1;
 IS_EMPLOYEE_ABSENT=0;
 IS_EMPLOYEE_FULLTIME=1;
-IS_EMPLOYEE_PARTTIME=0
-
+IS_EMPLOYEE_PARTTIME=0;
+WORKING_DAYS_PER_MONTH=20;
 #Variable
 JobType=0;
 Salary_Of_Employee=0;
@@ -23,16 +23,18 @@ case $Random_No_Check in
 		    then
 		    	#If JobType value is '1'.
 			echo "This is Full Time Employee.)"
-			Wage_Per_Hour=20
-			Full_Day_Hour=8
+			Wage_Per_Hour=20;
+			Full_Day_Hour=8;
 			#calculate total salary of employee
-			Salary_Of_Employee=$(( $Wage_Per_Hour * $Full_Day_Hour ))
+			Day_Salary_Of_Employee=$(( $Wage_Per_Hour * $Full_Day_Hour ))
+			Month_Salary_Of_Employee=$(($Day_Salary_Of_Employee * $WORKING_DAYS_PER_MONTH ))
 		    else
 			echo "This is Part Time Employee"
-			Wage_Per_Hour=20
-			Half_Day_Hour=4
+			Wage_Per_Hour=20;
+			Half_Day_Hour=4;
 			#Wage calculation for part time employee
-			Salary_Of_Employee=$(( $Wage_Per_Hour * $Half_Day_Hour ))
+			HalfDay_Salary_Of_Employee=$(( $Wage_Per_Hour * $Half_Day_Hour ))
+			Month_Salary_Of_Employee=$(( $HalfDay_Salary_Of_Employee * $WORKING_DAYS_PER_MONTH))
 		    fi
 		;;
 
@@ -40,7 +42,9 @@ case $Random_No_Check in
 		#If random_No_Check value is '0'.
 		echo "Employee is absent"
 		Salary_Of_Employee=0;
+		Month_Salary_Of_Employee=0;
 		;;
 
 esac
-echo "Your salary is creadited to your account,Rupees" $Salary_Of_Employee
+echo "Your today's salary is,Rupees" $Salary_Of_Employee
+echo "Your Monthly Salary creadited to your account,Rupees" $Month_Salary_Of_Employee
