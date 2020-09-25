63aeafd6125f9d6b31d605490a45995ce8edc7b9
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-25 19:09:13 +0530

    Checking part time employee salary

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -10,18 +10,22 @@ IS_EMPLOYEE_PARTTIME=0
 Random_No_Check=$(($RANDOM%2));
 if [[ $IS_EMPLOYEE_PRESENT -eq $Random_No_Check ]];
 then
+		#If Random_No_check value is '1'
+		echo "Employee is present"
 		JobType=$(($RANDOM%2))
 		if [[ $IS_EMPLOYEE_FULLTIME -eq $JobType ]]
 		then
-			#If Random_No_Check value is '1'.
-			echo " Employee is present"
+			#If JobType value is '1'.
+			echo "This is Full Time Employee"
 			Wage_Per_Hour=20;
 			Full_Day_Hour=8;
 			#calculate total salary of employee
 			Salary_Of_Employee=$(( $Wage_Per_Hour * $Full_Day_Hour ))
 		else
+			echo "This is Part Time Employee"
 			Wage_Per_Hour=20;
 			Half_Day_Hour=4;
+			#Wage calculation for part time employee
 			Salary_Of_Employee=$(( $Wage_Per_Hour * $Half_Day_Hour ))
 		fi
 else
