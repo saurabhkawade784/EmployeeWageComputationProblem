d2f9057d0efd4068aa21ba8fc3458b2ff4d63238
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-26 11:39:26 +0530

    Ok

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -6,46 +6,41 @@ echo "Welcome To EmployeeWageComputaion program"
 IS_EMPLOYEE_PRESENT=1;
 IS_EMPLOYEE_ABSENT=0;
 IS_EMPLOYEE_FULLTIME=1;
-IS_EMPLOYEE_PARTTIME=0;
-WAGE_PER_HOUR=20;
-WORKING_DAYS_PER_MONTH=20;
-MAX_WORKING_HOURSE=100;
-MAX_WORKING_DAYS=20;
+IS_EMPLOYEE_PARTTIME=0
+
 #Variable
-Emp_Total_Working_Hourse=0;
-Emp_Total_Working_Days=0;
+JobType=0;
+Salary_Of_Employee=0;
+Wage_Per_Hour=0;
+Full_Day_Hour=0;
+Half_Day_Hour=0;
+Random_No_Check=$(($RANDOM%2));
+case $Random_No_Check in
+	$IS_EMPLOYEE_PRESENT)#If Random_No_check value is '1'
+		    echo "Employee is present"
+		    JobType=$(($RANDOM%2))
+		    if [[ $IS_EMPLOYEE_FULLTIME -eq $JobType ]]
+		    then
+		    	#If JobType value is '1'.
+			echo "This is Full Time Employee.)"
+			Wage_Per_Hour=20
+			Full_Day_Hour=8
+			#calculate total salary of employee
+			Salary_Of_Employee=$(( $Wage_Per_Hour * $Full_Day_Hour ))
+		    else
+			echo "This is Part Time Employee"
+			Wage_Per_Hour=20
+			Half_Day_Hour=4
+			#Wage calculation for part time employee
+			Salary_Of_Employee=$(( $Wage_Per_Hour * $Half_Day_Hour ))
+		    fi
+		;;
 
-while [[ $Emp_Total_Working_Hourse -lt $MAX_WORKING_HOURSE && $Emp_Total_Working_Days -lt $MAX_WORKING_DAYS ]]
-do
-	((Emp_Total_Working_Days++));
-	JobType=$(($RANDOM%2));
-	case $JobType in
-		$IS_EMPLOYEE_FULLTIME)
-	    	#If JobType value is '1'.
-	    	echo "This is Full Time Employee."
-	    	Day_Hour=8;
-	    	;;
-		$IS_EMPLOYEE_PARTTIME)
-	    	#If JobType value is '2'.
-	    	echo "This is Part Time Employee."
-	    	Day_Hour=4;
-	    	;;
-		*) Day_Hour=0; ;;
+	$IS_EMPLOYEE_ABSENT)
+		#If random_No_Check value is '0'.
+		echo "Employee is absent"
+		Salary_Of_Employee=0;
+		;;
 
-	esac
-
-	Emp_Total_Working_Hourse=$(( $Emp_Total_Working_Hourse + $WAGE_PER_HOUR ));
-
-
-done
-
-#calculation of employee wage for one day.
-#Day_Salary_Of_Employee=$(( $WAGE_PER_HOUR * $Day_Hour ))
-#calculation of employee wage for month.
-#Month_Salary_Of_Employee=$(($Day_Salary_Of_Employee * $WORKING_DAYS_PER_MONTH ))
-#echo "Value of : " $Day_Hour
-#echo "Your today's salary is,Rupees" $Salary_Of_Employee
-#echo "Your Monthly Salary creadited to your account,Rupees" $Month_Salary_Of_Employee
-Employee_Total_Salary=$(($Emp_Total_Working_Hourse * $WAGE_PER_HOUR ))
-echo "Employee salary for 100 hrs and 20 days is: " $Employee_Total_Salary
-
+esac
+echo "Your salary is creadited to your account,Rupees" $Salary_Of_Employee
