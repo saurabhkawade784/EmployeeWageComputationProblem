92d14d65f2c6b6d2e2a1654e58c99e6d0f7372bd
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-26 11:23:52 +0530

    ok

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -14,26 +14,31 @@ MAX_WORKING_DAYS=20;
 #Variable
 Emp_Total_Working_Hourse=0;
 Emp_Total_Working_Days=0;
+
 while [[ $Emp_Total_Working_Hourse -lt $MAX_WORKING_HOURSE && $Emp_Total_Working_Days -lt $MAX_WORKING_DAYS ]]
 do
 	((Emp_Total_Working_Days++));
 	JobType=$(($RANDOM%2));
 	case $JobType in
 		$IS_EMPLOYEE_FULLTIME)
-		    #If JobType value is '1'.
-		    echo "This is Full Time Employee."
-		    Day_Hour=8;
-		    ;;
+	    	#If JobType value is '1'.
+	    	echo "This is Full Time Employee."
+	    	Day_Hour=8;
+	    	;;
 		$IS_EMPLOYEE_PARTTIME)
-		    #If JobType value is '2'.
-		    echo "This is Part Time Employee."
-		    Day_Hour=4;
-		    ;;
-					*) Day_Hour=0; ;;
+	    	#If JobType value is '2'.
+	    	echo "This is Part Time Employee."
+	    	Day_Hour=4;
+	    	;;
+		*) Day_Hour=0; ;;
 
 	esac
-	Emp_Total_Working_Hourse=$(( $Emp_Total_Working_Hourse + $Day_Hour ));
+
+	Emp_Total_Working_Hourse=$(( $Emp_Total_Working_Hourse + $WAGE_PER_HOUR ));
+
+
 done
+
 #calculation of employee wage for one day.
 #Day_Salary_Of_Employee=$(( $WAGE_PER_HOUR * $Day_Hour ))
 #calculation of employee wage for month.
@@ -43,3 +48,4 @@ done
 #echo "Your Monthly Salary creadited to your account,Rupees" $Month_Salary_Of_Employee
 Employee_Total_Salary=$(($Emp_Total_Working_Hourse * $WAGE_PER_HOUR ))
 echo "Employee salary for 100 hrs and 20 days is: " $Employee_Total_Salary
+
